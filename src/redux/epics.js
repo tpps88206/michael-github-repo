53a91bac382b30bcf654d9dfc00f05f80802da86
Sheng-Chuan Hsu <cool96482@hotmail.com>
2020-11-08 14:08:22 +0800

    test prod file

--- src/redux/epics.js
+++ src/redux/epics.js
@@ -1,7 +1,6 @@
 import { combineEpics } from 'redux-observable';
 
-import * as general from '@/redux/slices/general';
 import * as search from '@/redux/slices/search';
 import { getEpicsFromSlices } from './utils';
 
-export default combineEpics(...getEpicsFromSlices([general, search]));
+export default combineEpics(...getEpicsFromSlices([search]));
--- src/redux/reducers.js
+++ src/redux/reducers.js
@@ -2,12 +2,10 @@ import { combineReducers } from 'redux';
 
 import { connectRouter } from 'connected-react-router';
 
-import general from '@/redux/slices/general';
 import search from '@/redux/slices/search';
 
 export default history =>
   combineReducers({
     router: connectRouter(history),
-    general,
     search,
   });
